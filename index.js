d994cd0e2d316e18b76d5fabcc82ad55e5a52a31
RajrupDasid <rajrupofficialid@protonmail.com>
2024-01-01 16:48:52 +0530

    micro arc index.js file
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./userRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
+
+app.use('/api/users', userRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
