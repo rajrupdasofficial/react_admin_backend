8cb6195869275df65d89058da661063ca9c86e1a
RajrupDasid <rajrupofficialid@protonmail.com>
2024-01-01 16:49:56 +0530

    port js deleted because it was unused
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- port.js
+++
@@ -1,9 +0,0 @@
-function serverport(){
-    const PORT = 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  }
-  
-  module.exports = serverport; // Remove the parentheses after serverport
-  
